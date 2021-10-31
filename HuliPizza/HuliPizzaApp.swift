ab4f8d33ec71e19f4b43f182d8aecba3b201320c
Leon <yanglyu.leon.7@gmail.com>
2021-10-30 19:59:03 -0700

    Initial Commit

---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Yang Lyu on 10/30/21.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
